d42e2239c42b741509fc306493ed55299d6001f1
Ayomidegol <153425359+Ayomidegol@users.noreply.github.com>
2023-12-10 11:12:22 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['2349123588034', 'AYOMIDE', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
